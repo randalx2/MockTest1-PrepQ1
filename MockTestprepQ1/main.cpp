8eaebc1969b2c3e896951c80d7645651fad4b40e
randalx2 <pmisthry@gmail.com>
2016-03-15 00:28:12 +0200

    Created prototypes and skeleton code

---
+++ MockTestprepQ1/main.cpp
@@ -0,0 +1,29 @@
+// ****** QUESTION 1 *****
+// Question 1 has 2 parts
+//
+// 1.1 Overload a set of functions "swap(x, y)" which take the names of two variables as arguments 
+//     and swap the values contained in the variables.   This should work for two integers and 
+//     two characters.  USE REFERENCES (NOT POINTERS)!!!
+// 1.2 Write driver code that demonstrates the use of your functions by sorting an array of 10 integers 
+//     and an array of 10 characters from lowest to highest. 
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+void swap(int, int);
+void swap(char, char);
+
+int main(){
+
+	system("PAUSE");
+	return 0;
+}
+
+void swap(int &x, int &y)
+{
+
+}
